ec4e94db4a1fe960ea6a5bda93cee818cebf4580
Mal <malrsteffes@gmail.com>
2019-09-12 19:22:58 -0400

    added a submit form. the submit button now has an on click function that pulls the value from the user input box, and pulls the pertaining GIFs from the API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,24 @@
 
 // Create an array to hold the animals
 
-// Grab our GIPHY API by making an Ajax request
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=86gmopU2iKrSWy2FWvm1h5sM3An49fxH&limit=5"
+// This .on("click") function will trigger the AJAX call
+$("#find-animal").on("click", function(event) {
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+    // event.preventDefault() can be used to prevent an event's default behavior.
+    // Here, it prevents the submit button from trying to submit a form when clicked
+    event.preventDefault();
+
+    // Here we grab the text from the input box
+    var animal = $("#animal-input").val();
+
+    // Grab our GIPHY API by making an Ajax request
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal +"&api_key=86gmopU2iKrSWy2FWvm1h5sM3An49fxH&limit=5"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+
+});
